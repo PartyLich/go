36c9a3175c2afb23bf2ec25458f2b1ef77a73c81
Arthur E. Jones <PartyLich@gmail.com>
2022-05-16 15:25:32 -0500

    feat: add RevIterator fluent api

---
+++ iter/revIterator.go
@@ -0,0 +1,29 @@
+package iter
+
+func (iter *RevIterator[T]) Count() int {
+	return Count[T](iter)
+}
+
+func (iter *RevIterator[T]) Partition(pred func(T) bool) ([]T, []T) {
+	return Partition[T](iter, pred)
+}
+
+func (iter *RevIterator[T]) Filter(pred func(T) bool) *Filtered[T] {
+	return Filter[T](iter, pred)
+}
+
+func (iter *RevIterator[T]) SkipWhile(pred func(T) bool) *SkipWhileT[T] {
+	return SkipWhile[T](iter, pred)
+}
+
+func (iter *RevIterator[T]) TakeWhile(pred func(T) bool) *TakeWhileT[T] {
+	return TakeWhile[T](iter, pred)
+}
+
+func (iter *RevIterator[T]) Chain(b Iterable[T]) *Chained[T] {
+	return Chain[T](iter, b)
+}
+
+func (iter *RevIterator[T]) StepBy(step int) *Stepped[T] {
+	return StepBy[T](iter, step)
+}
