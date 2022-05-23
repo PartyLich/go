dad936bcfa8f4bd972b3af879130caeb0011207b
Arthur E. Jones <PartyLich@gmail.com>
2022-05-22 23:56:59 -0500

    chore: move Map definitions to own file

--- iter/iter.go
+++ iter/iter.go
@@ -104,37 +104,6 @@ func (iter *RevIterator[T]) Find(pred func(T) bool) *T {
 	return nil
 }
 
-type Mapped[T any, O any] struct {
-	iter Iterable[T]
-	fn   func(T) O
-}
-
-// Map returns an iterator that applies a function to every element.
-func Map[T any, O any](iter Iterable[T], fn func(T) O) *Mapped[T, O] {
-	return &Mapped[T, O]{iter, fn}
-}
-
-func (m *Mapped[T, O]) Next() *O {
-	next := m.iter.Next()
-
-	if next == nil {
-		return nil
-	}
-	result := m.fn(*next)
-
-	return &result
-}
-
-func (iter *Mapped[T, O]) Find(pred func(O) bool) *O {
-	for next := iter.Next(); next != nil; next = iter.Next() {
-		if pred(*next) {
-			return next
-		}
-	}
-
-	return nil
-}
-
 // Reduce repeatedly applies a reducing operation, reducing the iterator to a
 // single element
 func Reduce[T any, O any](iter Iterable[T], init O, fn func(O, T) O) O {
--- iter/map.go
+++ iter/map.go
@@ -1,5 +1,36 @@
 package iter
 
+type Mapped[T any, O any] struct {
+	iter Iterable[T]
+	fn   func(T) O
+}
+
+// Map returns an iterator that applies a function to every element.
+func Map[T any, O any](iter Iterable[T], fn func(T) O) *Mapped[T, O] {
+	return &Mapped[T, O]{iter, fn}
+}
+
+func (m *Mapped[T, O]) Next() *O {
+	next := m.iter.Next()
+
+	if next == nil {
+		return nil
+	}
+	result := m.fn(*next)
+
+	return &result
+}
+
+func (iter *Mapped[T, O]) Find(pred func(O) bool) *O {
+	for next := iter.Next(); next != nil; next = iter.Next() {
+		if pred(*next) {
+			return next
+		}
+	}
+
+	return nil
+}
+
 func (iter *Mapped[T, O]) Count() int {
 	return Count[O](iter)
 }
