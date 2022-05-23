1a3c44e307778619b01e0e245262e392527b858c
Arthur E. Jones <PartyLich@gmail.com>
2022-05-23 00:06:04 -0500

    chore: move SkipWhile definitions to own file

--- iter/iter.go
+++ iter/iter.go
@@ -87,43 +87,6 @@ func Nth[T any](iter Iterable[T], n int) *T {
 	return next
 }
 
-// SkipWhile iterable adapter
-type SkipWhileT[T any] struct {
-	iter Iterable[T]
-	flag bool
-	pred func(T) bool
-}
-
-// SkipWhile creates an iterator that skips elements based on a predicate.
-func SkipWhile[T any](iter Iterable[T], pred func(T) bool) *SkipWhileT[T] {
-	return &SkipWhileT[T]{iter, false, pred}
-}
-
-func (s *SkipWhileT[T]) Next() *T {
-	check := func(flag *bool, pred func(T) bool) func(T) bool {
-		return func(t T) bool {
-			if *flag || !pred(t) {
-				*flag = true
-				return true
-			} else {
-				return false
-			}
-		}
-	}
-
-	return s.iter.Find(check(&s.flag, s.pred))
-}
-
-func (iter *SkipWhileT[T]) Find(pred func(T) bool) *T {
-	for next := iter.Next(); next != nil; next = iter.Next() {
-		if pred(*next) {
-			return next
-		}
-	}
-
-	return nil
-}
-
 // Partition consumes an iterator, creating two slices from it.
 //
 // The first slice contains all of the elements for which the predicate returned
