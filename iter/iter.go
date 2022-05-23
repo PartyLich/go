a6cc1c9fa0c36fbfaa7531afd9dbb009c17749c4
Arthur E. Jones <PartyLich@gmail.com>
2022-05-23 00:07:14 -0500

    chore: move TakeWhile definitions to own file

--- iter/iter.go
+++ iter/iter.go
@@ -110,47 +110,6 @@ type Chainer[T any] interface {
 	Chain(Iterable[T]) Iterable[T]
 }
 
-// TakeWhile iterable adapter
-type TakeWhileT[T any] struct {
-	iter Iterable[T]
-	flag bool
-	pred func(T) bool
-}
-
-// TakeWhile Creates an iterator that yields elements based on a predicate.
-//
-// TakeWhile takes a predicate function as an argument. It will call this
-// function on each element of the iterator, and yield elements while it returns
-// true.
-func TakeWhile[T any](iter Iterable[T], pred func(T) bool) *TakeWhileT[T] {
-	return &TakeWhileT[T]{iter, true, pred}
-}
-
-func (s *TakeWhileT[T]) Next() *T {
-	check := func(flag *bool, pred func(T) bool) func(T) bool {
-		return func(t T) bool {
-			if *flag && pred(t) {
-				return true
-			} else {
-				*flag = false
-				return false
-			}
-		}
-	}
-
-	return s.iter.Find(check(&s.flag, s.pred))
-}
-
-func (iter *TakeWhileT[T]) Find(pred func(T) bool) *T {
-	for next := iter.Next(); next != nil; next = iter.Next() {
-		if pred(*next) {
-			return next
-		}
-	}
-
-	return nil
-}
-
 // All tests if every element of the iterator matches a predicate.
 //
 // All takes a function that returns true or false. It applies this function to
