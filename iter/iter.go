f7824a8c7700a22412c905328fd3a3d262000184
Arthur E. Jones <PartyLich@gmail.com>
2022-05-22 23:59:24 -0500

    chore: move RevIterator definitions to own file

--- iter/iter.go
+++ iter/iter.go
@@ -51,10 +51,6 @@ func (iter *Iterator[T]) Find(pred func(T) bool) *T {
 	return nil
 }
 
-type RevIterator[T any] struct {
-	it Iterator[T]
-}
-
 // Rev reverses the iteration order of this iterator
 func (iter *Iterator[T]) Rev() *RevIterator[T] {
 	var idx int
@@ -69,39 +65,6 @@ func (iter *Iterator[T]) Rev() *RevIterator[T] {
 	return &RevIterator[T]{
 		Iterator[T]{idx, iter.slice},
 	}
-}
-
-func (iter *RevIterator[T]) Next() *T {
-	if iter.it.idx < 0 {
-		return nil
-	}
-
-	next := &iter.it.slice[iter.it.idx]
-	iter.it.idx -= 1
-
-	return next
-}
-
-// Find searches for an element of an iterator that satisfies a predicate.
-//
-// takes a function that returns `true` or `false`. It applies this function to
-// each element of the iterator, and if any of them return `true`, then Find
-// returns a pointer to the element. If they all return `false`, it returns
-// `nil`.
-//
-// Find is short-circuiting; in other words, it will stop processing as soon as
-// the closure returns `true`.
-func (iter *RevIterator[T]) Find(pred func(T) bool) *T {
-	for iter.it.idx >= 0 {
-		next := iter.it.slice[iter.it.idx]
-		iter.it.idx--
-
-		if pred(next) {
-			return &next
-		}
-	}
-
-	return nil
 }
 
 // Reduce repeatedly applies a reducing operation, reducing the iterator to a
--- iter/revIterator.go
+++ iter/revIterator.go
@@ -1,5 +1,42 @@
 package iter
 
+type RevIterator[T any] struct {
+	it Iterator[T]
+}
+
+func (iter *RevIterator[T]) Next() *T {
+	if iter.it.idx < 0 {
+		return nil
+	}
+
+	next := &iter.it.slice[iter.it.idx]
+	iter.it.idx -= 1
+
+	return next
+}
+
+// Find searches for an element of an iterator that satisfies a predicate.
+//
+// takes a function that returns `true` or `false`. It applies this function to
+// each element of the iterator, and if any of them return `true`, then Find
+// returns a pointer to the element. If they all return `false`, it returns
+// `nil`.
+//
+// Find is short-circuiting; in other words, it will stop processing as soon as
+// the closure returns `true`.
+func (iter *RevIterator[T]) Find(pred func(T) bool) *T {
+	for iter.it.idx >= 0 {
+		next := iter.it.slice[iter.it.idx]
+		iter.it.idx--
+
+		if pred(next) {
+			return &next
+		}
+	}
+
+	return nil
+}
+
 func (iter *RevIterator[T]) Count() int {
 	return Count[T](iter)
 }
