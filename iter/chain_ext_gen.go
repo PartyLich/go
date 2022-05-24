78464c5b62e8b98fdd47fdd37ad725e8ac996fbf
Arthur E. Jones <PartyLich@gmail.com>
2022-05-23 20:43:41 -0500

    chore: use codegen for Chain fluent api

---
+++ iter/chain_ext_gen.go
@@ -0,0 +1,59 @@
+// Code generated from template. May be overwritten if modified manually. DO NOT EDIT.
+
+package iter
+
+func (iter *Chained[T]) Count() int {
+	return Count[T](iter)
+}
+
+func (iter *Chained[T]) Partition(pred func(T) bool) ([]T, []T) {
+	return Partition[T](iter, pred)
+}
+
+func (iter *Chained[T]) Filter(pred func(T) bool) *Filtered[T] {
+	return Filter[T](iter, pred)
+}
+
+func (iter *Chained[T]) SkipWhile(pred func(T) bool) *SkipWhileT[T] {
+	return SkipWhile[T](iter, pred)
+}
+
+func (iter *Chained[T]) TakeWhile(pred func(T) bool) *TakeWhileT[T] {
+	return TakeWhile[T](iter, pred)
+}
+
+func (iter *Chained[T]) Chain(b Iterable[T]) *Chained[T] {
+	return Chain[T](iter, b)
+}
+
+func (iter *Chained[T]) StepBy(step int) *Stepped[T] {
+	return StepBy[T](iter, step)
+}
+
+func (iter *Chained[T]) Skip(n int) *Skipped[T] {
+	return Skip[T](iter, n)
+}
+
+func (iter *Chained[T]) Take(n int) *Taken[T] {
+	return Take[T](iter, n)
+}
+
+func (iter *Chained[T]) Collect() []T {
+	return Collect[T](iter)
+}
+
+func (iter *Chained[T]) ForEach(fn func(T)) {
+	ForEach[T](iter, fn)
+}
+
+func (iter *Chained[T]) Nth(n int) *T {
+	return Nth[T](iter, n)
+}
+
+func (iter *Chained[T]) All(pred func(T) bool) bool {
+	return All[T](iter, pred)
+}
+
+func (iter *Chained[T]) Any(pred func(T) bool) bool {
+	return Any[T](iter, pred)
+}
