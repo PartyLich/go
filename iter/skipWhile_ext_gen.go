4bb18b7c7f8caf03f5d7fe3c13098dfb687667da
Arthur E. Jones <PartyLich@gmail.com>
2022-05-26 16:50:28 -0500

    docs: add doc comments to generated code

--- iter/skipWhile_ext_gen.go
+++ iter/skipWhile_ext_gen.go
@@ -15,58 +15,125 @@ func (iter *SkipWhileT[T]) Find(pred func(T) bool) *T {
 	return Find[T](iter, pred)
 }
 
+// Count consumes the iterator, counting the number of iterations and returning
+// it.
 func (iter *SkipWhileT[T]) Count() int {
 	return Count[T](iter)
 }
 
+// Partition consumes an iterator, creating two slices from it.
+//
+// The first slice contains all of the elements for which the predicate returned
+// true, and the second slice contains all of the elements for which it returned
+// false.
 func (iter *SkipWhileT[T]) Partition(pred func(T) bool) ([]T, []T) {
 	return Partition[T](iter, pred)
 }
 
+// Filter returns an iterator which uses a predicate function to determine if an
+// element should be yielded.
+//
+// The returned iterator will yield only the elements for which the predicate
+// returns true.
 func (iter *SkipWhileT[T]) Filter(pred func(T) bool) *Filtered[T] {
 	return Filter[T](iter, pred)
 }
 
+// SkipWhile creates an iterator that skips elements based on a predicate.
 func (iter *SkipWhileT[T]) SkipWhile(pred func(T) bool) *SkipWhileT[T] {
 	return SkipWhile[T](iter, pred)
 }
 
+// TakeWhile Creates an iterator that yields elements based on a predicate.
 func (iter *SkipWhileT[T]) TakeWhile(pred func(T) bool) *TakeWhileT[T] {
 	return TakeWhile[T](iter, pred)
 }
 
+// Chain takes two iterators and creates a new iterator over both in sequence.
+//
+// Chain will return a new iterator which will first iterate over values from
+// the first iterator and then over values from the second iterator.
 func (iter *SkipWhileT[T]) Chain(b Iterable[T]) *Chained[T] {
 	return Chain[T](iter, b)
 }
 
+// StepBy creates an iterator starting at the same point, but stepping by the
+// given amount at each iteration.
+//
+// The method will panic if the given step is <= 0.
+//
+// Note 1: The first element of the iterator will always be returned, regardless
+// of the step given.
 func (iter *SkipWhileT[T]) StepBy(step int) *Stepped[T] {
 	return StepBy[T](iter, step)
 }
 
+// Skip creates an iterator that skips the first n elements.
 func (iter *SkipWhileT[T]) Skip(n int) *Skipped[T] {
 	return Skip[T](iter, n)
 }
 
+// Take creates an iterator that yields the first n elements, or fewer if the
+// underlying iterator ends sooner.
 func (iter *SkipWhileT[T]) Take(n int) *Taken[T] {
 	return Take[T](iter, n)
 }
 
+// Collect transforms an iterator into a slice.
 func (iter *SkipWhileT[T]) Collect() []T {
 	return Collect[T](iter)
 }
 
+// ForEach calls a function on each element of an iterator.
+//
+// This is equivalent to using a for loop on the iterator, although break and
+// continue are not possible.
 func (iter *SkipWhileT[T]) ForEach(fn func(T)) {
 	ForEach[T](iter, fn)
 }
 
+// Nth returns the nth element of the iterator.
+//
+// Like most indexing operations, the count starts from zero, so Nth(0)
+// returns the first value, nth(1) the second, and so on.
+//
+// Note that all preceding elements, as well as the returned element, will be
+// consumed from the iterator. That means that the preceding elements will be
+// discarded, and also that calling Nth(0) multiple times on the same iterator
+// will return different elements.
+//
+// Nth will return nil if n is greater than or equal to the length of the
+// iterator.
 func (iter *SkipWhileT[T]) Nth(n int) *T {
 	return Nth[T](iter, n)
 }
 
+// All tests if every element of the iterator matches a predicate.
+//
+// All takes a function that returns true or false. It applies this function to
+// each element of the iterator, and if they all return true, then so does All.
+// If any of them return false, it returns false.
+//
+// All is short-circuiting; in other words, it will stop processing as soon as
+// it finds a false, given that no matter what else happens, the result will
+// also be false.
+//
+// An empty iterator returns true.
 func (iter *SkipWhileT[T]) All(pred func(T) bool) bool {
 	return All[T](iter, pred)
 }
 
+// Any tests if any element of the iterator matches a predicate.
+//
+// Any takes a function that returns true or false. It applies this function to
+// each element of the iterator, and if any of them return true, then so does
+// Any. If they all return false, it returns false.
+//
+// Any is short-circuiting; in other words, it will stop processing as soon as
+// it finds a true, given that no matter what else happens, the result will also
+// be true.
+//
+// An empty iterator returns false.
 func (iter *SkipWhileT[T]) Any(pred func(T) bool) bool {
 	return Any[T](iter, pred)
 }
